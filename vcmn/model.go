b11c8ac74f2db28e406c82cf5fa9dae45f0825aa
Varun Amachi <varunamachi@gmail.com>
2018-02-18 19:31:20 +0530

    Simplified generic param

--- vcmn/model.go
+++ vcmn/model.go
@@ -28,3 +28,43 @@ type DateRange struct {
 func (r *DateRange) IsValid() bool {
 	return !(r.From.IsZero() || r.To.IsZero())
 }
+
+//ParamType - type of the parameter
+type ParamType int
+
+const (
+	//Boolean - bool parameter
+	Boolean ParamType = iota
+
+	//NumberRange - number range parameter
+	NumberRange
+
+	//Choice - parameter with choices
+	Choice
+
+	//Text - arbitrary string
+	Text
+)
+
+//Pair - association of key and value
+type Pair struct {
+	Key   string `json:"key" bson:"key"`
+	Value string `json:"value" bson:"value"`
+}
+
+//Range - integer range
+type Range struct {
+	Min int `json:"min" bson:"min"`
+	Max int `json:"max" bson:"max"`
+}
+
+//Param - represents generic parameter
+type Param struct {
+	Name    string      `json:"name" bson:"name"`
+	Type    ParamType   `json:"type" bson:"type"`
+	Desc    string      `json:"desc" bson:"desc"`
+	Value   interface{} `json:"value" bson:"value"`
+	Range   Range       `json:"range" bson:"range"`
+	Choices []Pair      `json:"choices" bson:"choices"`
+	Default interface{} `json:"default" bson:"default"`
+}
